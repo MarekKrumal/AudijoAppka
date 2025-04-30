69741ff0ec24fe0441e688ee2c551aa549e40604
MarekKrumal <krumalmarek@gmail.com>
2025-04-30 22:02:27 +0200

    BookListItem

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import "./global.css";
+import { View } from "react-native";
+import books from "./src/dummyBooks";
+import { StatusBar } from "expo-status-bar";
+
+import BookListItem from "./src/components/BookListItem";
 
 export default function App() {
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-green-400">Audio appka tailwind setup</Text>
+    <View className="bg-gray-950 flex-1  justify-center p-4">
+      <BookListItem book={books[0]} />
+      <BookListItem book={books[1]} />
+      <BookListItem book={books[2]} />
+      <BookListItem book={books[3]} />
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/components/BookListItem.tsx
@@ -0,0 +1,30 @@
+import { Image, Text, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+  thumbnail_url?: string;
+  audio_url: string;
+};
+
+type BookListItemProps = {
+  book: Book;
+};
+
+export default function BookListItem({ book }: BookListItemProps) {
+  return (
+    <View className="flex-row gap-4 items-center">
+      <Image
+        source={{ uri: book.thumbnail_url }}
+        className="w-16 aspect-square rounded-md"
+      />
+      <View className="flex-1 gap-1">
+        <Text className="text-green-400 text-2xl font-bold">{book.title}</Text>
+        <Text className="text-green-400">{book.author}</Text>
+      </View>
+      <AntDesign name="playcircleo" size={24} color="lightgreen" />
+    </View>
+  );
+}
